62cb42867ee12f42e49fbd5f19b584cdaa0ce994
Nadav <nadav.matalon@gmail.com>
2016-11-08 20:56:59 +0000

    code formating

--- Switch.cpp
+++ Switch.cpp
@@ -44,64 +44,75 @@ _doubleClick       ____________________________________________| |____
 */
 
 #if ARDUINO >= 100
-  #include "Arduino.h"
+    #include "Arduino.h"
 #else
-  #include "WProgram.h"
+    #include "WProgram.h"
 #endif
+
 #include "Switch.h"
 
 // level(0)
 Switch::Switch(byte _pin, byte PinMode, bool polarity, int debounceDelay, int longPressDelay, int doubleClickDelay):
 pin(_pin), polarity(polarity), debounceDelay(debounceDelay), longPressDelay(longPressDelay), doubleClickDelay(doubleClickDelay)
-{ pinMode(pin, PinMode);
-  _switchedTime = millis();
-  level = digitalRead(pin);
+{
+    pinMode(pin, PinMode);
+    _switchedTime = millis();
+    level = digitalRead(pin);
 }
 
 bool Switch::poll()
-{ _longPress = _doubleClick = false;
-  bool newlevel = digitalRead(pin);
-
-  if(!longPressLatch)
-  { _longPress = on() && ((long)(millis() - pushedTime) > longPressDelay); // true just one time between polls
-    longPressLatch = _longPress; // will be reset at next switch
-  }
-
-  if((newlevel != level) & (millis() - _switchedTime >= debounceDelay))
-  { _switchedTime = millis();
-    level = newlevel;
-    _switched = 1;
-    longPressLatch = false;
-
-    if(pushed())
-    { _doubleClick = (long)(millis() - pushedTime) < doubleClickDelay;
-      pushedTime = millis();
+{
+    _longPress = _doubleClick = false;
+    bool newlevel = digitalRead(pin);
+    if(!longPressLatch)
+    {
+        _longPress = on() && ((long)(millis() - pushedTime) > longPressDelay);  // true just one time between polls
+        longPressLatch = _longPress;                                            // will be reset at next switch
     }
-    return _switched;
-  }
-  return _switched = 0;
+
+    if((newlevel != level) & (millis() - _switchedTime >= debounceDelay))
+    {
+        _switchedTime = millis();
+        level = newlevel;
+        _switched = 1;
+        longPressLatch = false;
+      
+        if(pushed())
+        {
+            _doubleClick = (long)(millis() - pushedTime) < doubleClickDelay;
+            pushedTime = millis();
+        }
+        return _switched;
+    }
+    return _switched = 0;
 }
 
 bool Switch::switched()
-{ return _switched;
+{
+    return _switched;
 }
 
 bool Switch::on()
-{ return !(level^polarity);
+{
+    return !(level^polarity);
 }
 
 bool Switch::pushed()
-{ return _switched && !(level^polarity);
+{
+    return _switched && !(level^polarity);
 }
 
 bool Switch::released()
-{ return _switched && (level^polarity);
+{
+    return _switched && (level^polarity);
 }
 
 bool Switch::longPress()
-{ return _longPress;
+{
+    return _longPress;
 }
 
 bool Switch::doubleClick()
-{ return _doubleClick;
+{
+    return _doubleClick;
 }
